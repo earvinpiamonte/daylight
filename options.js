b3c81da213b925aa01498af7a2e3068d8cfbc521
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-09-19 00:58:56 +0800

    Update options page

--- options.js
+++ options.js
@@ -10,30 +10,40 @@ function loadEventListeners() {
   const $closeWindowBtn = document.querySelector(".app-close-window");
   const $submitOptionsBtn = document.querySelector(".app-submit-options");
 
+  const $resetNotes = document.querySelector("#app-reset-notes");
+  const $notesTemplate = document.querySelector("#app-notes-template");
+
   $closeWindowBtn.addEventListener("click", goBack);
   $submitOptionsBtn.addEventListener("click", submitOptions);
+
+  /* $resetNotes.addEventListener("input", () => {
+    $notesTemplate.toggleAttribute("readonly");
+  }); */
 }
 
 function submitOptions() {
   const $notesTemplate = document.querySelector("#app-notes-template");
-  const $saveNotes = document.querySelector("#app-save-notes");
-  const doSaveNotes = $saveNotes.checked;
+  const $resetNotes = document.querySelector("#app-reset-notes");
 
   chromeSetData("notesTemplate", $notesTemplate.value);
-  chromeSetData("saveNotes", doSaveNotes);
+  chromeSetData("resetNotes", $resetNotes.checked);
 
   alert("Options successfully saved.");
 }
 
 function restoreOptions() {
   const $notesTemplate = document.querySelector("#app-notes-template");
-  const $saveNotes = document.querySelector("#app-save-notes");
+  const $resetNotes = document.querySelector("#app-reset-notes");
 
   chromeGetData("notesTemplate", (data) => {
     $notesTemplate.value = data;
   });
 
-  chromeGetData("saveNotes", (data) => {
-    $saveNotes.checked = data;
+  chromeGetData("resetNotes", (data) => {
+    $resetNotes.checked = data;
+
+    /* if (!$resetNotes.checked) {
+      $notesTemplate.setAttribute("readonly", $resetNotes.checked);
+    } */
   });
 }
