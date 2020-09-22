704e1c1e0c8e86c33820fc2123dfc2036faed5f4
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2020-09-23 02:26:28 +0800

    Add `[first_load_time]` variable for Template #20

--- helper.js
+++ helper.js
@@ -24,6 +24,18 @@ const getTimestamp = () => {
   return now.getTime();
 };
 
+const getDateTime = (date) => {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+
+  let time = hours + ":" + minutes + " " + ampm;
+  return time.toUpperCase();
+};
+
 const newLine = (multiplier = 1) => {
   let newLine = "";
 
@@ -83,9 +95,12 @@ const decodeVariable = (string) => {
   }
 
   const randomNumber = Math.floor(Math.random() * daylightLyrics.length);
+  const today = new Date();
+  const firstLoadTime = getDateTime(today);
 
   string = string.replaceAll("[current_date]", getCurrentFullDate());
   string = string.replaceAll("[daylight]", daylightLyrics[randomNumber]);
+  string = string.replaceAll("[first_load_time]", firstLoadTime);
 
   return string;
 };
